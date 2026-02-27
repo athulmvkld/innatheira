cb09a856b04e663db79f5dc56a912778c9faf74d
athulmvkld <124667551+athulmvkld@users.noreply.github.com>
2026-02-27 08:27:20 +0000

    bug fix2

--- app.py
+++ app.py
@@ -638,5 +638,4 @@ def reset_persons():
 
 # if __name__ == "__main__":
 #     app.run(debug=True, port=5000)
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app = Flask(__name__)
